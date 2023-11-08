4bce4b88b15af05ab6faeea02696fc96c49a6e47
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-11-08 09:04:32 +0700

    feat: refactor role permission and add middleware

---
+++ app/Http/Middleware/CheckWriter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class CheckWriter
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user_id = Auth::user()->id;
+        $user = User::find($user_id);
+        if ($user->role_id != 2) {
+            return redirect()->route('welcome');
+        }
+        return $next($request);
+    }
+}
