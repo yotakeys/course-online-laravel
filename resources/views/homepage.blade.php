1b6371afaf127b2eaddbfe59f466f24a1123e54c
Alfan Lukeyan Rizki <alfanlukeyanrizki@gmail.com>
2023-11-22 12:37:27 +0700

    feat: add landing section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,9 +3,62 @@
 @section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Hello, Again!") }}
+            <div class="bg-primary overflow-hidden">
+                <div class="flex items-center">
+                    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-primary overflow-hidden sm:rounded-lg ">
+                        <div>
+                            <p>Hello, Again!</p>
+                            <h1 class="text-left pb-6 text-3xl font-semibold">Join experienced Learning on this platform.</h1>
+                        </div>
+                        <!-- Session Status -->
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <!-- Email Address -->
+                            <div>
+                                <x-input-label for="email" :value="__('Email')" />
+                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+
+                            <!-- Password -->
+                            <div class="mt-4">
+                                <x-input-label for="password" :value="__('Password')" />
+
+                                <x-text-input id="password" class="block mt-1 w-full"
+                                                type="password"
+                                                name="password"
+                                                required autocomplete="current-password" />
+
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+
+                            <!-- Remember Me -->
+                            <div class="block mt-4">
+                                <label for="remember_me" class="inline-flex items-center">
+                                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                </label>
+                            </div>
+
+                            <div class="flex items-center justify-end mt-4">
+                                @if (Route::has('password.request'))
+                                    <a class="underline text-sm text-gray-600 hover:text-secondary rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                @endif
+
+                                <x-primary-button class="ml-3">
+                                    {{ __('Log in') }}
+                                </x-primary-button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="">
+                        <img src="{{ asset('images/graphic.png') }}" alt="Graphic" />
+                    </div>
                 </div>
             </div>
         </div>
