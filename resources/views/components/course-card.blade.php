ad63883541a96e9afdf14b8c8f5e63016c07d1dc
Alfan Lukeyan Rizki <alfanlukeyanrizki@gmail.com>
2023-11-30 18:44:34 +0700

    feat: header when scrolled change color

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,6 +1,6 @@
 <div class="relative group h-full w-full flex flex-col">
     <div class="relative p-3 z-10 w-full h-full rounded-xl border border-black p-1 bg-white transition-transform duration-200 ease-in-out transform group-hover:translate-x-2 group-hover:-translate-y-2 flex flex-col">
-        <div class="rounded-xl p-2 {{ $plan == 'FREE' ? 'bg-[#EAFCC9]' : 'bg-yellow-500' }} text-center">{{$plan}}</div>
+        <div class="rounded-xl p-2 {{ $plan == 'Free' ? 'bg-[#EAFCC9]' : 'bg-yellow-500' }} text-center">{{$plan}}</div>
         <div class="text-left pt-4 pb-4">
             <h1 class="text-left pb-2 text-2xl font-semibold ">{{$title}}</h1>
             <div class="w-full">{{$description}}</div>
